442e54e92d191e9c7e32e3841cc608e83fb283c9
Victor Yang <victoryang@Victors-M1Pro-3.local>
2024-03-20 14:05:58 -0500

    init

---
+++ src/main/java/hongtay/vy/discgolfdataapi/DiscGolfDataApiApplication.java
@@ -0,0 +1,13 @@
+package hongtay.vy.discgolfdataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscGolfDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscGolfDataApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hongtay/vy/discgolfdataapi/DiscGolfDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hongtay.vy.discgolfdataapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscGolfDataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
